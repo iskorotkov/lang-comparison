e3fed2024c6e1b1dd40d5fe026cbc74fc68adf06
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-13 20:29:34 +0500

    Add project files.

---
+++ LangComparison.Cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LangComparison.Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
